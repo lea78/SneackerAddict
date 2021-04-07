e8b31512e7e3c633619ae69976f683b84c42ed30
DjouliCorn <tissier.julie@gmail.com>
2021-04-08 00:04:28 +0200

    premiere tentative carousel

---
+++ app/src/main/java/com/lea/sneaker_addict/SliderItem.java
@@ -0,0 +1,14 @@
+package com.lea.sneaker_addict;
+
+public class SliderItem {
+
+    private int image;
+
+    SliderItem(int image){
+        this.image = image;
+    }
+
+    public int getImage(){
+        return image;
+    }
+}
