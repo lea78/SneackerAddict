0bd0fe8724fdfaadabb31f3d3c2e7e60802906b1
DjouliCorn <tissier.julie@gmail.com>
2021-04-08 11:05:40 +0200

    fini cardview pour carousel

--- app/src/main/java/com/lea/sneaker_addict/MainActivity.java
+++ app/src/main/java/com/lea/sneaker_addict/MainActivity.java
@@ -1,44 +1,19 @@
 package com.lea.sneaker_addict;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
+
 
 import android.os.Bundle;
-import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
 
-        viewPager2 = findViewById(R.id.artiste_slider);
-
-        List<SliderItem> sliderItems = new ArrayList<>();
-        sliderItems.add(new SliderItem((R.drawable.artiste1)));
-        sliderItems.add(new SliderItem((R.drawable.artiste2)));
-        sliderItems.add(new SliderItem((R.drawable.artiste3)));
-
-        viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
-
-        viewPager2.setClipToPadding(false);
-        viewPager2.setClipChildren(false);
-        viewPager2.setOffscreenPageLimit(4);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
-        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-        viewPager2.setPageTransformer(compositePageTransformer);
 
     }
 }
