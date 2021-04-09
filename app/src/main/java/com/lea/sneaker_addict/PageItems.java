885701ca6762218107a8412c73d5d5d4dc365a48
lea78 <lea.hamzi@edu.itescia.fr>
2021-04-09 16:57:37 +0200

    debut page single item

---
+++ app/src/main/java/com/lea/sneaker_addict/PageItems.java
@@ -0,0 +1,14 @@
+package com.lea.sneaker_addict;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageItems extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produit_page);
+    }
+}
