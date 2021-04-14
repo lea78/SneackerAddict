83407abbb9f7d40d3ae8123d748f359ba175642f
DjouliCorn <tissier.julie@gmail.com>
2021-04-14 16:59:43 +0200

    navigation dans recycler view et button back sur product page

--- app/src/main/java/com/lea/sneaker_addict/adapters/AllProductsAdapter.java
+++ app/src/main/java/com/lea/sneaker_addict/adapters/AllProductsAdapter.java
@@ -1,43 +1,55 @@
 package com.lea.sneaker_addict.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.lea.sneaker_addict.R;
+import com.lea.sneaker_addict.activities.ProductPageActivity;
 
 public class AllProductsAdapter extends RecyclerView.Adapter<AllProductsAdapter.MyViewHolder> {
 
     String data1[], data2[];
     int img[];
     Context cont;
+    private RecyclerViewClickListener listener;
 
-    public AllProductsAdapter(Context myCont, String nomItem[], String descItem[], int images[]){
+    public AllProductsAdapter(Context myCont, String nomItem[], String descItem[], int images[], RecyclerViewClickListener listener){
         cont = myCont;
         data1 = nomItem;
         data2 = descItem;
         img = images;
+        this.listener = listener;
 
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
 
         TextView textName, textDesc;
         ImageView imgShoes;
+        RelativeLayout mainAllProducts;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textName =itemView.findViewById(R.id.nom_sneakers);
             textDesc =itemView.findViewById(R.id.desc_sneakers);
             imgShoes = itemView.findViewById(R.id.img_sneakers);
-
+            mainAllProducts = itemView.findViewById(R.id.layout_main_tousLesProduits);
+            itemView.setOnClickListener(this);
         }
-        
+
+        @Override
+        public void onClick(View v) {
+            listener.onClick(v, getAbsoluteAdapterPosition());
+        }
     }
 
     @NonNull
@@ -60,5 +72,8 @@ public class AllProductsAdapter extends RecyclerView.Adapter<AllProductsAdapter.
         return data1.length;
     }
 
+    public interface RecyclerViewClickListener{
+        void onClick(View v, int position);
+    }
 
 }
