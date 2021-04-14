44f3899bfd09d24cab75398f7442184d997b7f5f
DjouliCorn <tissier.julie@gmail.com>
2021-04-14 10:57:32 +0200

    correction bug

--- app/src/main/java/com/lea/sneaker_addict/activities/LoginActivity.java
+++ app/src/main/java/com/lea/sneaker_addict/activities/LoginActivity.java
@@ -1,15 +1,17 @@
-package com.lea.sneaker_addict;
+package com.lea.sneaker_addict.activities;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.lea.sneaker_addict.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
     }
 }
