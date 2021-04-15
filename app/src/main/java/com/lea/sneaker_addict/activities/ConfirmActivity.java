dee30510aea64ee40b6e69b056e5f333af0ea5dc
DjouliCorn <tissier.julie@gmail.com>
2021-04-15 12:26:14 +0200

    finish page confirmation xml

---
+++ app/src/main/java/com/lea/sneaker_addict/activities/ConfirmActivity.java
@@ -0,0 +1,17 @@
+package com.lea.sneaker_addict.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lea.sneaker_addict.R;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.component_confirm);
+    }
+}
