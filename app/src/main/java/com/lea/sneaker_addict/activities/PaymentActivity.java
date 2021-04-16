4c596f0050258da9c458196c154423cf8b6a7a84
DjouliCorn <tissier.julie@gmail.com>
2021-04-16 22:51:09 +0200

    moyen de paiment ajout bottom nav

--- app/src/main/java/com/lea/sneaker_addict/activities/PaymentActivity.java
+++ app/src/main/java/com/lea/sneaker_addict/activities/PaymentActivity.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lea.sneaker_addict.R;
 
 public class PaymentActivity extends AppCompatActivity {
@@ -21,55 +24,83 @@ public class PaymentActivity extends AppCompatActivity {
     public EditText mEditPayment;
     public RadioButton rButtonPaypal, rButtonCreditCard;
     public Button buttonPayment;
-    public String confirmPayment ="";
+    public String confirmPayment = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.component_payment);
+        setContentView(R.layout.activity_payment);
 
-        buttonPayment = (Button)findViewById(R.id.button_moyen_paiement);
-        rButtonPaypal = (RadioButton)findViewById(R.id.paypal);
-        rButtonCreditCard = (RadioButton)findViewById(R.id.info_carte);
+        buttonPayment = (Button) findViewById(R.id.button_moyen_paiement);
+        rButtonPaypal = (RadioButton) findViewById(R.id.paypal);
+        rButtonCreditCard = (RadioButton) findViewById(R.id.info_carte);
+
         buttonPayment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(rButtonPaypal.isChecked()){
+                if (rButtonPaypal.isChecked()) {
                     Intent intent = new Intent(PaymentActivity.this, ConfirmActivity.class);
                     intent.putExtra("payment_change", "Paypal");
                     startActivity(intent);
                 }
-                if (rButtonCreditCard.isChecked()){
+                if (rButtonCreditCard.isChecked()) {
                     Intent intent = new Intent(PaymentActivity.this, ConfirmActivity.class);
-                    mEditPayment = (EditText)findViewById(R.id.edit_num_carte);
+                    mEditPayment = (EditText) findViewById(R.id.edit_num_carte);
                     String strPayment = mEditPayment.getText().toString();
                     String maskPayment = maskCardNumber(strPayment, "xxxx-xxxx-xxxx-####");
                     String kindOfPayment = "Carte de crédit";
-                    confirmPayment = kindOfPayment + "\n" +maskPayment;
+                    confirmPayment = kindOfPayment + "\n" + maskPayment;
                     intent.putExtra("payment_change", confirmPayment);
                     startActivity(intent);
                 }
             }
         });
+
+        //*****BOTTOM NAVIGATION BAR*****//
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_paiement);
+        bottomNavigationView.setSelectedItemId(R.id.menu_produit);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.menu_homepage:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.menu_produit:
+                        return true;
+
+                    case R.id.menu_profil:
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 
+    //Method for button back
     public void back(View view) {
         startActivity(new Intent(PaymentActivity.this, ConfirmActivity.class));
     }
 
-    //Function to mask the number of the credit card
-    public String maskCardNumber(String cardNumber, String mask){
+    //Method to mask the number of the credit card
+    public String maskCardNumber(String cardNumber, String mask) {
         int index = 0;
         StringBuilder maskedNumber = new StringBuilder();
-        for (int i = 0; i < mask.length(); i++){
+        for (int i = 0; i < mask.length(); i++) {
             char c = mask.charAt(i);
-            if(c == '#'){
+            if (c == '#') {
                 maskedNumber.append(cardNumber.charAt(index));
                 index++;
-            } else if(c == 'x') {
+            } else if (c == 'x') {
                 maskedNumber.append(c);
                 index++;
-            }else{
+            } else {
                 maskedNumber.append(c);
             }
         }
