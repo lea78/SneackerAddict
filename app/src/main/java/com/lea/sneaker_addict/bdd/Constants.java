9c309908df806771216bff4fbea0c260a5bb231e
lea78 <lea.hamzi@edu.itescia.fr>
2021-04-17 13:03:34 +0200

    mail problème réglé + accès denied réglé. Mais "Failed to connect" sur les liens http des script php.

--- app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
+++ app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
@@ -1,7 +1,8 @@
 package com.lea.sneaker_addict.bdd;
 
 public class Constants {
-    private static final String ROOT_URL = "http://192.168.1.35:8888/sneakersAddict/webService/";
+    private static final String ROOT_URL = "http://127.0.0.1/sneakersAddict/webService/";
+    //private static final String ROOT_URL = "http://192.168.1.35/sneakersAddict/webService/";
     public static final String URL_REGISTER = ROOT_URL+"RegisterUser.php";
 
 }
