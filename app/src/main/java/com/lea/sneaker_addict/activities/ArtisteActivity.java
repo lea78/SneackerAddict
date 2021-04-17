3a50c02676027f9cf7836d3844ec3ff639119948
DjouliCorn <tissier.julie@gmail.com>
2021-04-17 20:26:09 +0200

    refonte viewpager2

--- app/src/main/java/com/lea/sneaker_addict/activities/ArtisteActivity.java
+++ app/src/main/java/com/lea/sneaker_addict/activities/ArtisteActivity.java
@@ -1,10 +1,15 @@
 package com.lea.sneaker_addict.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.lea.sneaker_addict.R;
 import com.lea.sneaker_addict.adapters.ArtistePageAdapter;
@@ -14,31 +19,15 @@ public class ArtisteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_artiste);
+        setContentView(R.layout.component_artistepage);
 
-        //3 - Configure ViewPager
-        this.configureViewPager();
+        TableLayout tableLayout = findViewById(R.id.activity_main_tabs);
+        TabItem tabApropos = findViewById(R.id.tab_apropos);
+        TabItem tabProduits = findViewById(R.id.tab_produits);
+        TabItem tabCommandes = findViewById(R.id.tab_commandes);
+        TabItem tabAvis = findViewById(R.id.tab_avis);
+        ViewPager viewPager = findViewById(R.id.viewpager_artiste_contenu);
+
     }
 
-    private void configureViewPager(){
-        // 1 - Get ViewPager from layout
-        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
-        // 2 - Set Adapter PageAdapter and glue it together
-        pager.setAdapter(new ArtistePageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager)) {
-        });
-    }
-
-    private void configureViewPagerAndTabs(){
-        //Get ViewPager from layout
-        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
-        //Set Adapter PageAdapter and glue it together
-        pager.setAdapter(new ArtistePageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagesViewPager)));
-
-        // 1 - Get TabLayout from layout
-        TabLayout tabs= (TabLayout)findViewById(R.id.activity_main_tabs);
-        // 2 - Glue TabLayout and ViewPager together
-        tabs.setupWithViewPager(pager);
-        // 3 - Design purpose. Tabs have the same width
-        tabs.setTabMode(TabLayout.MODE_FIXED);
-    }
 }
--- app/src/main/java/com/lea/sneaker_addict/adapters/ArtistePageAdapter.java
+++ app/src/main/java/com/lea/sneaker_addict/adapters/ArtistePageAdapter.java
@@ -1,49 +1,25 @@
 package com.lea.sneaker_addict.adapters;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.lea.sneaker_addict.activities.AllProductsActivity;
-import com.lea.sneaker_addict.helpers.AproposOngletFragment;
-import com.lea.sneaker_addict.helpers.ArtistePageFragment;
+public class ArtistePageAdapter extends FragmentStateAdapter {
 
-public class ArtistePageAdapter extends FragmentPagerAdapter {
+    private static final int card_item_size = 4;
+    public ArtistePageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
 
-
-    // 1 - Array of colors that will be passed to PageFragment
-    private int[] colors;
-
-    // 2 - Default Constructor
-    public ArtistePageAdapter(FragmentManager mgr, int[] colors) {
-        super(mgr);
-        this.colors = colors;
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
     }
 
     @Override
-    public int getCount() {
-        return (5); // 3 - Number of page to show
+    public int getItemCount() {
+        return card_item_size;
     }
-
-    @Override
-    public Fragment getItem(int position) {
-        switch (position){
-            case 0: //Page number 1
-                return ArtistePageFragment.newInstance(position, 1);
-            case 1: //Page number 2
-                return AproposOngletFragment.newInstance();
-            //case 2: //Page number 3
-                //return AllProductsActivity.newInstance();
-            default:
-                return null;
-        }
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return "Page " + position;
-    }
-
-
-
 }
