8c4f9a0a0d8355b6830bcd605a5e71c9330224c4
DjouliCorn <tissier.julie@gmail.com>
2021-04-19 01:41:26 +0200

    commencement recycler suite

--- app/src/main/java/com/lea/sneaker_addict/adapters/AvisArtistePageAdapter.java
+++ app/src/main/java/com/lea/sneaker_addict/adapters/AvisArtistePageAdapter.java
@@ -1,4 +1,65 @@
 package com.lea.sneaker_addict.adapters;
 
-public class AvisArtistePageAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lea.sneaker_addict.R;
+
+public class AvisArtistePageAdapter extends RecyclerView.Adapter<AvisArtistePageAdapter.MyViewHolder> {
+    Context context;
+    String nomClient[], commentaireClient[], dateAvis[];
+    int imageClient[];
+
+    public AvisArtistePageAdapter(Context mContext, String mNomClient[], String mCommentaireClient[], String mDateAvis[], int mImageClient[]){
+        context = mContext;
+        nomClient = mNomClient;
+        commentaireClient = mCommentaireClient;
+        dateAvis = mDateAvis;
+        imageClient = mImageClient;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View myViewInflater = inflater.inflate(R.layout.item_pageartiste_avis, parent, false);
+        return new AvisArtistePageAdapter.MyViewHolder(myViewInflater);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.nomClientViewHolder.setText(nomClient[position]);
+        holder.commentaireViewHolder.setText(commentaireClient[position]);
+        holder.dateViewHolder.setText(dateAvis[position]);
+        holder.imageClientViewHolder.setImageResource(imageClient[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return nomClient.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView nomClientViewHolder, commentaireViewHolder, dateViewHolder;
+        ImageView imageClientViewHolder;
+        FrameLayout mainAvisViewHolder;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nomClientViewHolder = itemView.findViewById(R.id.pageArtiste_nomClient);
+            commentaireViewHolder = itemView.findViewById(R.id.pageArtiste_avisClient);
+            dateViewHolder = itemView.findViewById(R.id.pageArtiste_dateAvis);
+            imageClientViewHolder = itemView.findViewById(R.id.pageArtiste_imgPhotoClient);
+            mainAvisViewHolder = itemView.findViewById(R.id.framelayout_avis_pageartiste);
+        }
+    }
 }
--- app/src/main/java/com/lea/sneaker_addict/helpers/AvisFragment.java
+++ app/src/main/java/com/lea/sneaker_addict/helpers/AvisFragment.java
@@ -5,21 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.lea.sneaker_addict.R;
+import com.lea.sneaker_addict.adapters.AvisArtistePageAdapter;
 
 public class AvisFragment extends Fragment {
 
     RecyclerView recyclerView;
-    TextView nomClient[], commentaireClient[], dateAvis[];
-    ImageView imageClient[]={};
+    String[] nomClient;
+    String[] commentaireClient;
+    String[] dateAvis;
+    int[] imageClient ={R.drawable.photoprofil_homme_2, R.drawable.photoprofil_femme_3, R.drawable.photoprofil_homme_1};
 
     public AvisFragment(){
     }
@@ -29,6 +34,14 @@ public class AvisFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_avis, container, false);
         recyclerView = view.findViewById(R.id.recycler_pageArtiste_avis);
+        nomClient = getResources().getStringArray(R.array.comments_clientname_artisteprofil);
+        commentaireClient = getResources().getStringArray(R.array.comments_comments_artisteprofil);
+        dateAvis = getResources().getStringArray(R.array.comments_avis_artisteprofil);
+        AvisArtistePageAdapter avisArtistePageAdapter = new AvisArtistePageAdapter(getContext(), nomClient, commentaireClient, dateAvis, imageClient);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(avisArtistePageAdapter);
+
         return view;
     }
 }
