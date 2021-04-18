9bb364d6c624e0729262b0f5c52c2ef945fe9052
lea78 <lea.hamzi@edu.itescia.fr>
2021-04-18 18:15:07 +0200

    "Failed to connect" sur les liens http des script php.

--- app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
+++ app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
@@ -1,8 +1,9 @@
 package com.lea.sneaker_addict.bdd;
 
 public class Constants {
-    private static final String ROOT_URL = "http://127.0.0.1/sneakersAddict/webService/";
-    //private static final String ROOT_URL = "http://192.168.1.35/sneakersAddict/webService/";
+    //private static final String ROOT_URL = "http://127.0.0.1/sneakersAddict/webService/";
+    //private static final String ROOT_URL = "http://10.0.2.2/sneakersAddict/webService/";
+    private static final String ROOT_URL = "http://192.168.1.35/sneakersAddict/webService/";
     public static final String URL_REGISTER = ROOT_URL+"RegisterUser.php";
 
 }
