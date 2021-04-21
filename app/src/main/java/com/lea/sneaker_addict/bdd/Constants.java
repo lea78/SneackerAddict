f2f66e368c26a5f23b2f6a163ed0e55d550ac694
DjouliCorn <tissier.julie@gmail.com>
2021-04-21 13:00:29 +0200

    ip demaaaarde

--- app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
+++ app/src/main/java/com/lea/sneaker_addict/bdd/Constants.java
@@ -3,7 +3,7 @@ package com.lea.sneaker_addict.bdd;
 public class Constants {
     //private static final String ROOT_URL = "http://127.0.0.1/sneakersAddict/webService/";
     //private static final String ROOT_URL = "http://10.0.2.2/sneakersAddict/webService/";
-    private static final String ROOT_URL = "http://192.168.1.17/sneakersAddict/webService/";
+    private static final String ROOT_URL = "http://192.168.1.25/sneakersAddict/webService/";
 
     public static final String URL_REGISTER = ROOT_URL+"RegisterUser.php";
     public static final String URL_LOGIN = ROOT_URL+"LoginUser.php";
